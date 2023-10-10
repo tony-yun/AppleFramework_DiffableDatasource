718c0d39f46d7785b13301b68d475b046ff79d65
tony-yun <lifeyun24@naver.com>
2023-10-10 16:49:36 +0900

    Initial Commit

---
+++ AppleFramework/FrameworkListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FrameworkListViewController.swift
+//  AppleFramework
+//
+//  Created by joonwon lee on 2022/04/24.
+//
+
+import UIKit
+
+class FrameworkListViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let list: [AppleFramework] = AppleFramework.list
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.delegate = self
+        navigationController?.navigationBar.topItem?.title = "☀️ Apple Frameworks"
+        
+        // Data, Presentation, Layout 새로운 방법 사용 wwdc
+        // snapshot: data
+        // diffable datasource: presentation
+        // compositional layout: layout
+        
+    }
+}
+
+extension FrameworkListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let framework = list[indexPath.item]
+        print(">>> selected: \(framework.name)")
+    }
+}
