8ad2a0c9271891404f5483efe0a2c2e7340179d5
tony-yun <lifeyun24@naver.com>
2023-10-10 23:49:40 +0900

    snapshot, diffable datasource, compositional layout

--- AppleFramework/FrameworkListViewController.swift
+++ AppleFramework/FrameworkListViewController.swift
@@ -13,17 +13,53 @@ class FrameworkListViewController: UIViewController {
     
     let list: [AppleFramework] = AppleFramework.list
     
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    
+    typealias Item = AppleFramework
+    enum Section {
+        case main
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        collectionView.delegate = self
-        navigationController?.navigationBar.topItem?.title = "☀️ Apple Frameworks"
-        
-        // Data, Presentation, Layout 새로운 방법 사용 wwdc
+
+        // Data, Presentation, Layout 새로운 방법 사용 wwdc2019
         // snapshot: data
         // diffable datasource: presentation
         // compositional layout: layout
         
+        // Presentation
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FrameworkCell", for: indexPath) as? FrameworkCell else {
+                return nil
+            }
+            cell.configure(itemIdentifier)
+            return cell
+        })
+        
+        // Data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(list, toSection: .main)
+        dataSource.apply(snapshot)
+        
+        // layout
+        collectionView.collectionViewLayout = layout()
+    }
+    
+    // layout
+    private func layout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.33), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.5))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item]) //?
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 10)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
     }
 }
 
